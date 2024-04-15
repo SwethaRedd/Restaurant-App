cdbd5306d0a4b09687603c49e004b35e8fa9d3a8
Swetha <swethareddy.nadipi@gmail.com>
2024-04-15 10:27:05 -0400

    res-app:static header nav

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-test
+# Restaurant App Layout
+
+/\*
+
+- Header
+  - Logo
+  - Nav Items - Home, About us, Cart
+- Body
+
+  - Search Component
+  - Cards Container
+    - Restaurant Card 1
+      - Image
+      - Description
+      - Cuisine
+      - Location
+      - Rating ----etc.
+    - Restaurant Card 2
+
+- Footer - Copy rights - Links - Contact Info
+  \*/
---
+++ app.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import logo from "./assets/logo.jpg";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={logo} alt="logo" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact US</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
