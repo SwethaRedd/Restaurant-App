b1d2265e498c9cd29efc86d271ac9048bda31f0a
Swetha <swethareddy.nadipi@gmail.com>
2024-05-01 15:20:45 -0400

    create browser router and config it to our app

--- src/app.js
+++ src/app.js
@@ -3,8 +3,13 @@ import ReactDOM from "react-dom/client";
 import Footer from "./components/Footer";
 import Body from "./components/Body";
 import Bod from "./components/Bod.js";
+import About from "./components/Pages/About.js";
+import Contact from "./components/Pages/Contact.js";
 import Banner from "./components/Banner.js";
 import Header from "./components/Header.js";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Contact from "./components/Pages/Contact.js";
+import Error from "./components/Pages/Error.js";
 
 const AppLayout = () => {
   return (
@@ -18,5 +23,26 @@ const AppLayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
+/**
+ * createBrowserRouter will take list of paths, path is an objectm will contians path & element
+ * RouterProvider will provide the routing config to our App.
+ *
+ */
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
