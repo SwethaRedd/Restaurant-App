e4e468f429d11fdf00037f0e5f08c299597af76a
Swetha <swethareddy.nadipi@gmail.com>
2024-05-01 11:47:37 -0400

    hook imp points

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../../assets/logo.jpg";
 
 const Header = () => {
   const btnName = "Login";
   const [buttonName, setButtonUpdate] = useState(btnName);
+  console.log("Header rendered");
+  /*
+  we import useEffect from named import from react library.
+  useEffect(() => {},[] );
+  
+  If no dependency array => useEffect is called on every render.
+  If dependency array is empty = [] => useEffect is called on initial render(just once)
+  if dependency array is [btnHandle] => useEffect is called everytume btnHandle is updated.
+  */
 
   const handleClick = () => {
     buttonName === "Login"
       ? setButtonUpdate("Logout")
       : setButtonUpdate("Login");
   };
+
+  useEffect(() => {
+    console.log("useEffect is called");
+  }, [handleClick]);
 
   return (
     <div className="header">
