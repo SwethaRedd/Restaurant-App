db919ef1b9fcb48433020ec3dfb806282d2f92ed
Swetha <swethareddy.nadipi@gmail.com>
2024-05-02 23:24:53 -0400

    add UserClass comp into About

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -1,11 +1,13 @@
 import React from "react";
 import User from "./User";
+import UserClass from "./UserClass";
 const About = () => {
   return (
     <div className="about">
       <h1>About</h1>
       <h2>I am About Component</h2>
       <User />
+      <UserClass />
     </div>
   );
 };
