db6bb870e5a18d3417f8dd9a166b140a751a7f37
Swetha <swethareddy.nadipi@gmail.com>
2024-05-02 23:08:03 -0400

    add User comp into About us

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -1,12 +1,16 @@
 import React from "react";
-
+import User from "./User";
 const About = () => {
   return (
-    <div>
+    <div className="about">
       <h1>About</h1>
       <h2>I am About Component</h2>
+      <User />
     </div>
   );
 };
 
 export default About;
+/**
+ *
+ */
