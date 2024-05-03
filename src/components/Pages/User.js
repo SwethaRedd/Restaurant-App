eae0be663aec7ad125274246a4ebdce326226614
Swetha <swethareddy.nadipi@gmail.com>
2024-05-02 23:07:41 -0400

    add functional User comp

---
+++ src/components/Pages/User.js
@@ -0,0 +1,11 @@
+const User = () => {
+  return (
+    <div className="user-card">
+      <h2>Name: Something</h2>
+      <h3>Location: Somewhere</h3>
+      <h4>Contact: something@gmail.com</h4>
+    </div>
+  );
+};
+
+export default User;
