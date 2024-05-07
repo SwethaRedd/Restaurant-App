dd432bde6e78729703cdd9900a7eaf8d966b76f3
Swetha <swethareddy.nadipi@gmail.com>
2024-05-07 13:50:12 -0500

    creating multiple state variables in functional component

--- src/components/Pages/User.js
+++ src/components/Pages/User.js
@@ -1,6 +1,10 @@
 const User = (props) => {
+  const [count] = useState(0);
+  const [count1] = useState(1);
   return (
     <div className="user-card">
+      <h1>Count = {count} </h1>
+      <h1>Count = {count1} </h1>
       <h2>Name: {props.name}</h2>
       <h3>Location: Somewhere</h3>
       <h4>Contact: something@gmail.com</h4>
