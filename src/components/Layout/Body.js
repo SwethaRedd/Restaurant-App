ab899c62f7d068752c45e13c07d4d00e0ae99812
Swetha <swethareddy.nadipi@gmail.com>
2024-05-22 00:02:19 -0400

    data format got updated in swiggy response

--- src/components/Layout/Body.js
+++ src/components/Layout/Body.js
@@ -36,10 +36,10 @@ const Body = () => {
     const json = await data.json();
     // console.log("json", json);
     setListOfRestaurants(
-      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
     setFilteredRestaurant(
-      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
 
     // Optional Chaining
