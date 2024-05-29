7ff364563a1ebbd41ff70db95bbe5a67cf8d64ab
Swetha <swethareddy.nadipi@gmail.com>
2024-05-29 19:48:15 -0400

    subscribing to the store using useSelector

--- src/components/Pages/Cart.js
+++ src/components/Pages/Cart.js
@@ -1,4 +1,13 @@
+import { useSelector } from "react-redux";
+
 const Cart = () => {
-  return <div> Cart </div>;
+  // Subscribing to the Store using useSelector - to get the whole store
+  const cartItems = useSelector((store) => store.cart.items);
+  //   console.log(cartItems);
+  return (
+    <div className="text-center m-4 p-4">
+      <h1 className="text-2xl font-bold">Cart </h1>
+    </div>
+  );
 };
 export default Cart;
