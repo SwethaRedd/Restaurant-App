8ad606d4f500dedf5724873b1a6a652f909ff369
Swetha <swethareddy.nadipi@gmail.com>
2024-05-29 19:49:28 -0400

    showing an ItemList in the cart page by passing the itemsList from the store

--- src/components/Pages/Cart.js
+++ src/components/Pages/Cart.js
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import ItemList from "../Restaurant/ItemList";
 
 const Cart = () => {
   // Subscribing to the Store using useSelector - to get the whole store
@@ -7,6 +8,9 @@ const Cart = () => {
   return (
     <div className="text-center m-4 p-4">
       <h1 className="text-2xl font-bold">Cart </h1>
+      <div>
+        <ItemList items={cartItems} />
+      </div>
     </div>
   );
 };
