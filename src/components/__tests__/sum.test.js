dbea6f859f76088c86fb12cb8d5a9f3d6b908935
Swetha <swethareddy.nadipi@gmail.com>
2024-06-06 15:00:42 -0400

    sample test case

--- src/components/__tests__/sum.test.js
+++ src/components/__tests__/sum.test.js
@@ -1,4 +1,4 @@
-import { sum } from "../sum";
+import { sum, sub } from "../sum";
 
 /*
 write test case:
@@ -13,4 +13,10 @@ test("Sum function should caculate the sum of two numbers", () => {
   const result = sum(3, 4); // call the function that you want to test
 
   expect(result).toBe(7); //Assertion : - here we are testing the value to be sum.
+});
+
+test("Sub function should caculate the sub of two numbers", () => {
+  const result = sub(4, 4); // call the function that you want to test
+
+  expect(result).toBe(0); //Assertion : - here we are testing the value to be sum.
 });
